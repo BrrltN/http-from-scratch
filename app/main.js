143d3c8c034de05c72be0c3e36de5260d1b68328
cruchon <barrouillet.nicolas@gmail.com>
2024-09-28 17:51:57 +0200

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -1,14 +1,93 @@
 const net = require("net");
 
+const HTTP_RESPONSE = {
+    PARSE_ERROR: "Invalid HTTP request\r\n\r\n",
+    OK: "HTTP/1.1 200 OK\r\n\r\n",
+    NOT_FOUND: "HTTP/1.1 404 NOT FOUND\r\n\r\n",
+}
 
-// Uncomment this to pass the first stage
-const server = net.createServer((socket) => {
-    socket.on("close", () => {
+/**
+ * @param { net.Socket } socket 
+ * @param { number } time 
+ */
+function enableTimeout(socket, time = 3000) {
+    socket.setTimeout(time)
+    socket.on('timeout', () => {
+        console.log('request is timeout')
         socket.end();
-    });
+    })
+}
 
-    socket.write("HTTP/1.1 200 OK\r\n\r\n");
-    socket.pipe(socket);
+/**
+ * @param { string | undefined } method 
+ */
+function isAvailableMethod(method) {
+    return method === "GET"
+        || method === "PUT"
+        || method === "POST"
+}
+
+/**
+ * @param { string | undefined } path 
+ */
+function isAvailablePath(path) {
+    const regex = /^\/[a-zA-Z]*$|^\*$/;
+
+    return regex.test(path)
+}
+
+/**
+ * @param { string | undefined } httpVersion 
+ */
+function isAvailableHttpVersion(httpVersion) {
+    return httpVersion === "HTTP/1.1"
+}
+
+/**
+ * 
+ * @param { string | undefined } method 
+ * @param { string | undefined } path 
+ * @param { string | undefined } httpVersion 
+ * @returns 
+ */
+function isAvailableStartLine(method, path, httpVersion) {
+    return isAvailableMethod(method)
+        && isAvailablePath(path)
+        && isAvailableHttpVersion(httpVersion)
+}
+
+const server = net.createServer((socket) => {
+    enableTimeout(socket);
+
+    let httpRequest = "";
+
+    socket.on("data", (data) => {
+        httpRequest += data.toString()
+        const clientRequestIsDone = httpRequest.endsWith("\r\n\r\n")
+        if (clientRequestIsDone) {
+            const lines = httpRequest.split("\r\n")
+
+            if (lines.length === 0) {
+                socket.end(HTTP_RESPONSE.PARSE_ERROR);
+            }
+
+            const startLine = lines[0]
+
+            const [method, path, httpVersion] = startLine.split(/\s+/);
+
+            if (!isAvailableStartLine(method, path, httpVersion)) {
+                socket.end(HTTP_RESPONSE.PARSE_ERROR);
+            }
+
+            if (path === "/") {
+                socket.write(HTTP_RESPONSE.OK);
+            }
+            else {
+                socket.write(HTTP_RESPONSE.NOT_FOUND);
+            }
+            socket.end();
+        }
+    })
 });
 
 server.listen(4221, "localhost");
